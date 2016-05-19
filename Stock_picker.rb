54522a3f07d205546df7e17ebb07fe5d2bed313e
urzp <urzp002@mail.ru>
2016-05-19 09:23:56 +0300

    New class Trade

---
+++ Stock_picker.rb
@@ -0,0 +1,15 @@
+#stock_picker([17,3,6,9,15,8,6,1,10])
+#=> [1,4]  # for a profit of $15 - $3 == $12
+
+class Trade
+	
+attr_accessor :days, :daly_prise
+
+	
+end
+
+
+
+my_trade=Trade.new
+my_trade.days=8
+my_trade.daly_prise=[17,3,6,9,15,8,6,1,10]
