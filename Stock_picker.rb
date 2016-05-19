c18eb9f422159165012cb774d1fd0b45ff91f12b
urzp <urzp002@mail.ru>
2016-05-19 11:07:35 +0300

    Make input data

--- Stock_picker.rb
+++ Stock_picker.rb
@@ -5,7 +5,10 @@ class Trade
 	
 attr_accessor :days, :daly_prise
 
+@daly_prise=:daly_prise
+
 	def stock_picker
+		
 		all_trades={}
 		day_buy=0
 		daly_prise.each do |buy|
@@ -20,6 +23,9 @@ attr_accessor :days, :daly_prise
 		benifit=all_trades.max_by{|potencial_trade,value| value}
 		benifit_days=benifit[0]
 		benifit_days
+		#puts benifit
+		
+		
 	end
 	
 end
@@ -27,5 +33,19 @@ end
 
 
 my_trade=Trade.new
-my_trade.days=8
-my_trade.daly_prise=[17,3,6,9,15,8,6,1,10]
+
+puts "How many trade days you want to add?"
+my_trade.days=gets.chomp.to_i
+input_daly_prise=Array.new(my_trade.days)
+
+0.upto(my_trade.days) do |day|
+puts "Put the price of the #{day} day"	
+input_daly_prise[day]=gets.chomp.to_i	
+end
+my_trade.daly_prise=input_daly_prise
+my_trade.stock_picker
+
+# test
+#my_trade.days=8
+#my_trade.daly_prise=[17,3,6,9,15,8,6,1,10]
+#my_trade.stock_picker
